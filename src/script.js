030290af74a07a112fd4beacccedd63a021900dd
Flora Hewitt <flora.stekelorom@gmail.com>
2019-04-02 18:58:36 +0400

    finished the todolist functionality

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
+class Task extends React.Component {
+  render () {
+    const { task, onDelete, onComplete } = this.props;
+    const { id, content, completed } = task;
+    return (
+      <div className="row mb-1">
+        <p className="col">{content}</p>
+        <button onClick={() => onDelete(id)}>Delete</button>
+        <input className="d-inline-block mt-2" type="checkbox" onChange={() => onComplete(id, completed)} checked={completed}/>
+      </div>
+    )
+  }
+}
+
+
 const checkStatus = (response) => {
   if (response.ok) {
     // .ok returns true if response status is 200-299
@@ -14,26 +29,139 @@ class Todolist extends React.Component {
     this.state = {
       newTask: '',
       tasks: [],
+      filter: 'all'
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.toggleComplete = this.toggleComplete.bind(this);
+    this.toggleFilter = this.toggleFilter.bind(this);
   }
+  componentDidMount() {
+    this.fetchTasks();
+  }
+  fetchTasks() {
+    fetch("https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=48")
+      .then(checkStatus)
+      .then(json)
+      .then((response) => {
+        console.log(response);
+        this.setState({tasks: response.tasks});
+      })
+      .catch(error => {
+        console.error(error.message);
+      })
+  }
+
   handleChange(event) {
     this.setState({ newTask: event.target.value });
   }
   handleSubmit(event) {
     event.preventDefault();
+    let { new_task } = this.state;
+    new_task = new_task.trim();
+    if (!new_task) {
+      return;
+    }
+    fetch("https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=48", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        task: {
+          content: new_task
+        }
+      }),
+    }).then(checkStatus)
+      .then(json)
+      .then((data) => {
+        this.setState({new_task: ''});
+        this.fetchTasks();
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
+    }
+    deleteTask(id) {
+    if (!id) {
+      return;
+    }
+    fetch(`https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=48`, {
+      method: "DELETE",
+      mode: "cors",
+    }).then(checkStatus)
+      .then(json)
+      .then((data) => {
+        this.fetchTasks();
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
   }
+  toggleComplete(id, completed) {
+    if (!id) {
+      return;
+    }
+    const newState = completed ? 'active' : 'complete';
+    fetch(`https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_${newState}?api_key=48`, {
+      method: "PUT",
+      mode: "cors",
+    }).then(checkStatus)
+      .then(json)
+      .then((data) => {
+        this.fetchTasks();
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
+  }
+  toggleFilter(e) {
+    console.log(e.target.name)
+    this.setState({
+      filter: e.target.name
+    })
+  }
+
   render () {
-    const { newTask, tasks } = this.state;
+    const { newTask, tasks, filter } = this.state;
     return (
       <div className="container">
         <div className="row">
           <div className="col-12">
             <h2 className="mb-3">To Do List</h2>
-            {tasks.length > 0 ? tasks.map((task) => {
-              return null;
+            {tasks.length > 0 ? tasks.filter(task => {
+              if (filter === 'all') {
+                return true;
+              } else if (filter === 'active') {
+                return !task.completed;
+              } else {
+                return task.completed;
+              }
+            }).map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  task={task}
+                  onDelete={this.deleteTask}
+                  onComplete={this.toggleComplete}
+                />
+              );
             }) : <p>no tasks here</p>}
+            <div className="mt-3">
+              <label>
+                <input type="checkbox" name="all" checked={filter === "all"} onChange={this.toggleFilter} /> All
+              </label>
+              <label>
+                <input type="checkbox" name="active" checked={filter === "active"} onChange={this.toggleFilter} /> Active
+              </label>
+              <label>
+                <input type="checkbox" name="completed" checked={filter === "completed"} onChange={this.toggleFilter} /> Completed
+              </label>
+            </div>
             <form onSubmit={this.handleSubmit} className="form-inline my-4">
               <input type="text" className="form-control mr-sm-2" placeholder="New task" value={newTask} onChange={this.handleChange}/>
               <button type="submit" className="btn btn-primary">Submit</button>
